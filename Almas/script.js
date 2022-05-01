5aebb5c17b57bff01d4678a004e6735970c7438f
Almas hussain <almashussain778@gmail.com>
2022-05-02 03:29:34 +0530

    Update script.js

--- Almas/script.js
+++ Almas/script.js
@@ -1,13 +1,14 @@
 var names=new Array();
-names[0]="Almas";
-names[1]="Amin";
-names[2]="Aman";
-names[3]="Naman";
-names[4]="Ajay";
-names[5]="Samai";
-names[6]="Abhishek";
-names[7]="joshi";
-names[8]="Aditya";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 for (var i = 0; i < names.length; i++){
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
